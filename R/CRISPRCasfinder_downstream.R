862492091b72c7a07f8b426f5c13d0e58e7ce00c
Asa12138 <3180105399@zju.edu.cn>
2023-05-18 21:15:40 +0800

    init 5.18

---
+++ R/CRISPRCasfinder_downstream.R
@@ -0,0 +1,94 @@
+#' Prepare the result files from CRISPRCasFinder
+#'
+#' @param input_folder the folder of CRISPRCasFinder result, contains GFF/,TSV/,result.json,...
+#' @param output_folder output, default: ./
+#' @param genome_name the genome_name
+#'
+#' @export
+#'
+#' @examples
+#' pre_CCF_res("extdata/AAB-S01R1_122","extdata")
+pre_CCF_res=function(input_folder,output_folder="./",genome_name=NULL){
+  if(!dir.exists(input_folder))stop("Can not find ",input_folder)
+  if(!dir.exists(output_folder))dir.create(output_folder)
+
+  if(is.null(genome_name))genome_name=basename(input_folder)
+
+  out_file1=paste0(output_folder,"/",genome_name,"_spacer.fa")
+  out_file2=paste0(output_folder,"/",genome_name,"_spacer_with_cas.fa")
+
+  #有annotation前缀的gff文件是有crispr array的contigs
+  crispr_gff_ls=list.files(paste0(input_folder,"/GFF"),pattern = "annotation*")%>%
+    gsub("annotation_","",.)
+
+  if(length(crispr_gff_ls)>0){
+    res=data.frame()
+    for (i in crispr_gff_ls) {
+      crispr_gff=paste0(input_folder,"/GFF/",i)
+      tmp=get_spacer(crispr_gff,genome_name=genome_name)
+      res=rbind(res,tmp)
+    }
+    write_fasta(res,out_file1)
+  } else {
+    pcutils::dabiao("No array found!")
+  }
+
+  #导出Cas信息
+  rawCas_file=paste0(input_folder,"/rawCas.fna")
+  if(file.exists(rawCas_file)){
+    cas_res=get_cas(rawCas_file,genome_name = genome_name)
+    write.csv(cas_res,file = paste0(output_folder,"/",genome_name,"_Cas_info.csv"),row.names = F)
+  }else {
+    pcutils::dabiao("No Cas found!")
+  }
+
+  #导出包含cas的spacer
+  df=read_fasta(out_file1)
+  spacer_with_cas=df[grepl(paste0(cas_res$seqid,collapse = "|"),df$Sequence_ID),]
+  write_fasta(spacer_with_cas,out_file2)
+  pcutils::dabiao("All done! see ",output_folder,"/")
+}
+
+get_spacer=function(crispr_gff,genome_name){
+  #用@分割，header要包括这些信息：spacer来源于哪条genome，哪条序列，是在该序列第几个crispr array 上，crispr array的起始和终止位点，再加一个spacer的起始位点，spacer的长度，以及是第几个spacer
+  res=data.frame(spacer_id=c(),spacer_seq=c())
+  gff1=read.table(crispr_gff,col.names = c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"))
+  gff2=filter(gff1,type%in%c("CRISPR","CRISPRspacer"))%>%select(1,3,4,5,9)
+  cid=0
+  for (r in 1:nrow(gff2)) {
+    row=gff2[r,]
+    if(row$type=="CRISPR"){
+      cid=cid+1
+      sid=0
+      crispr1=row
+    }
+    else if(row$type=="CRISPRspacer") {
+      sid=sid+1
+      attri=strsplit(row$attributes,";")[[1]]
+      spacer_id=paste0(genome_name,
+                       "@",row$seqid,
+                       "@CRISPR:",cid,
+                       "@",crispr1$start,"-",crispr1$end,
+                       "@",gsub("Name=","",attri[2]),"_",sid)
+      spacer_seq=gsub("sequence=","",attri[1])
+      # if(is.null(res))res=paste0(spacer_id,"\n",spacer_seq)
+      # else res=paste0(res,"\n",spacer_id,"\n",spacer_seq)
+      res=rbind(res,data.frame(spacer_id=spacer_id,spacer_seq=spacer_seq))
+    }
+  }
+  return(res)
+}
+
+get_cas=function(rawCas_file,genome_name){
+  #输出含有 cas protein 的 array
+  arrary_with_cas_id=readLines(rawCas_file)%>%grep("^>",.,value = T)
+  arrary_with_cas=pcutils::strsplit2(arrary_with_cas_id,"\\|")
+  arrary_cas=pcutils::strsplit2(arrary_with_cas$V4,"[ ,]")
+  res=cbind(arrary_with_cas[,c(2,3)],arrary_cas)
+  colnames(res)=c("seqid","type","subtype","start","end")
+  res$seqid=gsub("_\\d+$","",res$seqid)
+  res=dplyr::distinct(res)
+  cas_res=cbind(genome=genome_name,res)
+  cas_res
+}
+
